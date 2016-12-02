ce8bc5d086edaaf5666d1e48e3788e290d4cb879
asemle <asemle@umass.edu>
2016-12-01 21:09:24 -0700

    finished careers view

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-angular.module('riskMatch', ['ui.bootstrap', 'ui.router']);
-// .config(function ($stateProvider, $urlRouterProvider) {
-//   $stateProvider
-// })
+angular.module('riskMatch', ['ui.bootstrap', 'ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('careers', {
+      url:'/careers',
+      templateUrl: "../views/careers.html",
+      // controller: 'packagesCtrl'
+    })
+    $urlRouterProvider
+      .otherwise('/');
+});
