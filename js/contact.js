c34e833323b6a92ad0837ab063df7d18bc9b1382
asemle <asemle@umass.edu>
2016-12-04 20:36:41 -0700

    directive template is finished

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+angular.module('riskMatch').directive('contactDirective', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: './views/contact.html',
+      // template: 'Name: {{customer.name}}<br /> Street: {{customer.street}}'
+      // link : function ($scope, element, attrs) { }
+    };
+});
---
+++ js/indexCtrl.js
@@ -0,0 +1,4 @@
+angular.module('riskMatch')
+.controller('indexCtrl', function($scope) {
+  
+})
