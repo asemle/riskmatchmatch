56df790b40bee85fde4de3d672e17312b44bf256
asemle <asemle@umass.edu>
2016-12-06 13:31:20 -0700

    got nav to work, finally

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,8 +1,80 @@
 angular.module('riskMatch')
-.controller('homeCtrl', function($scope, newsService) {
+.controller('homeCtrl', function($scope, $element, newsService, $location, $anchorScroll, anchorSmoothScroll, $window, $document) {
+  $scope.scrollTo = function(id) {
+     $location.hash(id);
+     $anchorScroll();
+  }
 
+  var element = $element.find('#someId');
+ $scope.getOffset = function () {
+  return element.offset().top;
+ };
+
+ var home, intro, brokers, carriers, media;
+  //  home = $element.find('#home').prop('offsetTop');
+   intro = $element.find('#intro').prop('offsetTop') - 150;
+   brokers = $element.find('#brokers').prop('offsetTop') - 150;
+   carriers = $element.find('#carriers').prop('offsetTop') - 150;
+   media = $element.find('#media').prop('offsetTop') - 150;
+
+$scope.firstsc = 'active';
+
+
+  $document.on('scroll', function() {
+      // do your things like logging the Y-axis
+        if($window.scrollY < intro) {
+          $scope.firstsc = 'active';
+          $scope.secondsc = null;
+          $scope.thirdsc = null;
+          $scope.fourthsc = null;
+          $scope.fifthsc = null;
+        }
+        else if($window.scrollY >= intro && $window.scrollY < brokers) {
+          $scope.firstsc = null;
+          $scope.secondsc = 'active';
+          $scope.thirdsc = null;
+          $scope.fourthsc = null;
+          $scope.fifthsc = null;
+        }
+        else if($window.scrollY >= brokers && $window.scrollY < carriers) {
+          $scope.firstsc = null;
+          $scope.secondsc = null;
+          $scope.thirdsc = 'active';
+          $scope.fourthsc = null;
+          $scope.fifthsc = null;
+        }
+        else if(carriers<=$window.scrollY && $window.scrollY < media) {
+          $scope.firstsc = null;
+          $scope.secondsc = null;
+          $scope.thirdsc = null;
+          $scope.fourthsc = 'active';
+          $scope.fifthsc = null;
+        }
+        else if($window.scrollY >= media) {
+          $scope.firstsc = null;
+          $scope.secondsc = null;
+          $scope.thirdsc = null;
+          $scope.fourthsc = null;
+          $scope.fifthsc = 'active';
+        }
+      // or pass this to the scope
+      $scope.$apply(function() {
+          $scope.pixelsScrolled = $window.scrollY;
+      })
+  });
 
     $scope.frontpagenews = newsService.frontpagenews;
+
+    $scope.gotoElement = function (eID){
+          // set the location.hash to the id of
+          // the element you wish to scroll to.
+          $location.hash(eID);
+
+          // call $anchorScroll()
+          anchorSmoothScroll.scrollTo(eID);
+
+        };
+
 //     "name": "IBA Northeast: Kapnick Insurance partners with Riskmatch",
 //     "url": "http://www.bing.com/cr?IG=F5714D4F9F504794BA2099A21D6D77D8&CID=190B6EC4BEDA6C2B16B96724BFEB6D49&rd=1&h=sI6-z2cYwpgI8yqqgB9WRGEK3uIS6XprV0H_gJ6dKK0&v=1&r=http%3a%2f%2fwww.ibamag.com%2fus%2fnews%2fbreaking-news%2fiba-northeast-kapnick-insurance-partners-with-riskmatch-41150.aspx&p=DevEx,5021.1",
 //     "image": {
